9b2c214d822d21708886e698ea34414da6c798ee
GaLicn <133291877+GaLicn@users.noreply.github.com>
2025-09-25 18:21:13 +0800

    频道卡完成

--- src/main/java/com/extendedae_plus/mixin/ae2/helpers/InterfaceLogicUpgradesMixin.java
+++ src/main/java/com/extendedae_plus/mixin/ae2/helpers/InterfaceLogicUpgradesMixin.java
@@ -10,8 +10,10 @@ import com.extendedae_plus.compat.UpgradeSlotCompat;
 import net.neoforged.fml.ModList;
 import com.extendedae_plus.util.ExtendedAELogger;
 import net.minecraft.world.item.Item;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,6 +28,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class InterfaceLogicUpgradesMixin implements CompatUpgradeProvider {
 
     @Shadow(remap = false)
+    @Final
+    @Mutable
     private IUpgradeInventory upgrades;
 
     @Unique
