342a9510d76cea70b7f2940ff7390b038e867a44
C-H716 <1536152356@qq.com>
2025-11-04 21:04:09 +0800

    gtl：新版翻倍支持

---
+++ src/main/java/com/extendedae_plus/mixin/ae2/autopattern/CraftingTreeProcessMixin.java
@@ -0,0 +1,39 @@
+package com.extendedae_plus.mixin.ae2.autopattern;
+
+import appeng.api.crafting.IPatternDetails;
+import appeng.api.networking.crafting.ICraftingService;
+import appeng.crafting.CraftingCalculation;
+import appeng.crafting.CraftingTreeNode;
+import appeng.crafting.CraftingTreeProcess;
+import appeng.crafting.inv.CraftingSimulationState;
+import com.extendedae_plus.api.smartDoubling.ICraftingSimulationStateExt;
+import com.extendedae_plus.api.smartDoubling.ICraftingTreeProcessExt;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@SuppressWarnings({"AddedMixinMembersNamePattern"})
+@Mixin(value = CraftingTreeProcess.class, remap = false, priority = 2000)
+public class CraftingTreeProcessMixin implements ICraftingTreeProcessExt {
+    @Unique private ICraftingService craftingService;
+
+    @Inject(method = "<init>",at = @At("RETURN"))
+    private void init(ICraftingService cc, CraftingCalculation job, IPatternDetails details, CraftingTreeNode craftingTreeNode, CallbackInfo ci) {
+        this.craftingService = cc;
+    }
+
+    @Inject(
+        method = "ultraFastRequest",
+        at = @At("HEAD")
+    )
+    private void bindSimulationState(CraftingSimulationState inv, long times, CallbackInfo ci) {
+        ((ICraftingSimulationStateExt) inv).setSourceProcess((CraftingTreeProcess) (Object) this);
+    }
+
+    @Override
+    public ICraftingService getCraftingService() {
+        return this.craftingService;
+    }
+}
