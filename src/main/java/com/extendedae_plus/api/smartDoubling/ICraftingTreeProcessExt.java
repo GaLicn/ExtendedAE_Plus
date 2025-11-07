d41b2faceb610ee358442228f6d724df4037ffc9
C-H716 <1536152356@qq.com>
2025-11-07 21:41:53 +0800

    修复相同输出不同倍率或样板其他不同时造成的NPE

--- src/main/java/com/extendedae_plus/api/smartDoubling/ICraftingTreeProcessExt.java
+++
@@ -1,7 +0,0 @@
-package com.extendedae_plus.api.smartDoubling;
-
-import appeng.api.networking.crafting.ICraftingService;
-
-public interface ICraftingTreeProcessExt {
-    ICraftingService getCraftingService();
-}
