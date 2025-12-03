813500eb4281f867a54d11a8dd233e1095642ad0
GaLi <zhangjl@longersec.com>
2025-12-03 17:55:17 +0800

    超级装配矩阵样板核心适配ui

---
+++ src/main/java/com/extendedae_plus/mixin/extendedae/container/ContainerAssemblerMatrixPatternSlotTrackerMixin.java
@@ -0,0 +1,31 @@
+package com.extendedae_plus.mixin.extendedae.container;
+
+import appeng.api.inventories.InternalInventory;
+import appeng.util.inv.AppEngInternalInventory;
+import com.glodblock.github.extendedae.common.tileentities.matrix.TileAssemblerMatrixPattern;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = "com.glodblock.github.extendedae.container.ContainerAssemblerMatrix$PatternSlotTracker")
+public abstract class ContainerAssemblerMatrixPatternSlotTrackerMixin {
+
+    @Shadow
+    @Final
+    private InternalInventory server;
+
+    @Shadow
+    @Final
+    @Mutable
+    private InternalInventory client;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void extendedae_plus$resizeClientInventory(TileAssemblerMatrixPattern host, CallbackInfo ci) {
+        this.client = new AppEngInternalInventory(this.server.size());
+    }
+}
+
