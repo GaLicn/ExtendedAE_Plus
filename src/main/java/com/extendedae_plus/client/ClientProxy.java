145d57370b5408be661118585763f09e3964aa41
GaLicn <133291877+GaLicn@users.noreply.github.com>
2025-12-13 18:56:51 +0800

    1.5.1

--- src/main/java/com/extendedae_plus/client/ClientProxy.java
+++ src/main/java/com/extendedae_plus/client/ClientProxy.java
@@ -4,6 +4,9 @@ import appeng.client.render.crafting.CraftingCubeModel;
 import appeng.init.client.InitScreens;
 import com.extendedae_plus.ExtendedAEPlus;
 import com.extendedae_plus.ae.screen.EntitySpeedTickerScreen;
+import com.extendedae_plus.client.render.crafting.EPlusCraftingCubeModelProvider;
+import com.extendedae_plus.content.crafting.EPlusCraftingUnitType;
+import com.extendedae_plus.hooks.BuiltInModelHooks;
 import com.extendedae_plus.init.ModItems;
 import com.extendedae_plus.init.ModMenuTypes;
 import com.extendedae_plus.items.materials.EntitySpeedCardItem;
@@ -30,6 +33,27 @@ public final class ClientProxy {
         // 注册 Item property
         ItemProperties.register(ModItems.ENTITY_SPEED_CARD.get(), ExtendedAEPlus.id("mult"),
                 (stack, world, entity, seed) -> (float) EntitySpeedCardItem.readMultiplier(stack));
+
+        // 注册五种形成态模型为内置模型
+        BuiltInModelHooks.addBuiltInModel(
+                ExtendedAEPlus.id("block/crafting/4x_accelerator_formed_v2"),
+                new CraftingCubeModel(new EPlusCraftingCubeModelProvider(EPlusCraftingUnitType.ACCELERATOR_4x)));
+
+        BuiltInModelHooks.addBuiltInModel(
+                ExtendedAEPlus.id("block/crafting/16x_accelerator_formed_v2"),
+                new CraftingCubeModel(new EPlusCraftingCubeModelProvider(EPlusCraftingUnitType.ACCELERATOR_16x)));
+
+        BuiltInModelHooks.addBuiltInModel(
+                ExtendedAEPlus.id("block/crafting/64x_accelerator_formed_v2"),
+                new CraftingCubeModel(new EPlusCraftingCubeModelProvider(EPlusCraftingUnitType.ACCELERATOR_64x)));
+
+        BuiltInModelHooks.addBuiltInModel(
+                ExtendedAEPlus.id("block/crafting/256x_accelerator_formed_v2"),
+                new CraftingCubeModel(new EPlusCraftingCubeModelProvider(EPlusCraftingUnitType.ACCELERATOR_256x)));
+
+        BuiltInModelHooks.addBuiltInModel(
+                ExtendedAEPlus.id("block/crafting/1024x_accelerator_formed_v2"),
+                new CraftingCubeModel(new EPlusCraftingCubeModelProvider(EPlusCraftingUnitType.ACCELERATOR_1024x)));
     }
 
     @SubscribeEvent
