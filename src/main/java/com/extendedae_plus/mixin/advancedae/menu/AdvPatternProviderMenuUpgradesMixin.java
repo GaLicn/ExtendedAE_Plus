361c922538681c390ebb80d9757730afb5396047
GaLi <3096147684@qq.com>
2026-08-20 16:31:39 +0800

    删除toolbox添加

--- src/main/java/com/extendedae_plus/mixin/advancedae/menu/AdvPatternProviderMenuUpgradesMixin.java
+++ src/main/java/com/extendedae_plus/mixin/advancedae/menu/AdvPatternProviderMenuUpgradesMixin.java
@@ -1,9 +1,7 @@
 package com.extendedae_plus.mixin.advancedae.menu;
 
 import appeng.menu.AEBaseMenu;
-import appeng.menu.ToolboxMenu;
 import com.extendedae_plus.api.bridge.CompatUpgradeProvider;
-import com.extendedae_plus.api.bridge.IUpgradableMenu;
 import com.extendedae_plus.compat.UpgradeSlotCompat;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.MenuType;
@@ -13,16 +11,14 @@ import net.pedroksl.advanced_ae.gui.advpatternprovider.AdvPatternProviderMenu;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /** 未安装 Applied Flux 时为 AdvancedAE 供应器菜单添加本地升级卡槽。 */
 @Mixin(value = AdvPatternProviderMenu.class, remap = false)
-public abstract class AdvPatternProviderMenuUpgradesMixin extends AEBaseMenu implements IUpgradableMenu {
+public abstract class AdvPatternProviderMenuUpgradesMixin extends AEBaseMenu {
     @Shadow @Final protected AdvPatternProviderLogic logic;
-    @Unique private ToolboxMenu eap$toolbox;
 
     protected AdvPatternProviderMenuUpgradesMixin(MenuType<?> menuType, int id, Inventory playerInventory, Object host) {
         super(menuType, id, playerInventory, host);
@@ -34,13 +30,7 @@ public abstract class AdvPatternProviderMenuUpgradesMixin extends AEBaseMenu imp
     private void eap$setupChannelCardUpgrades(
             MenuType<?> menuType, int id, Inventory playerInventory, AdvPatternProviderLogicHost host, CallbackInfo ci) {
         if (UpgradeSlotCompat.shouldManageLocalUpgradeInventory()) {
-            this.eap$toolbox = new ToolboxMenu(this);
             this.setupUpgrades(((CompatUpgradeProvider) this.logic).eap$getCompatUpgrades());
         }
-    }
-
-    @Override
-    public ToolboxMenu eap$getToolbox() {
-        return this.eap$toolbox;
     }
 }
