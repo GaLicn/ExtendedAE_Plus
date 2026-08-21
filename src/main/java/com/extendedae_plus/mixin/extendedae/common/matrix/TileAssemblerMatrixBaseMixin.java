b78d671b35b4fc8140471752772bc709e37542b7
GaLi <3096147684@qq.com>
2026-08-21 21:45:00 +0800

    超装节点频道问题修复

--- src/main/java/com/extendedae_plus/mixin/extendedae/common/matrix/TileAssemblerMatrixBaseMixin.java
+++ src/main/java/com/extendedae_plus/mixin/extendedae/common/matrix/TileAssemblerMatrixBaseMixin.java
@@ -1,5 +1,6 @@
 package com.extendedae_plus.mixin.extendedae.common.matrix;
 
+import appeng.api.networking.IGridNode;
 import com.extendedae_plus.content.matrix.supermatrix.SuperAssemblerMatrixCalculator;
 import com.extendedae_plus.content.matrix.supermatrix.SuperAssemblerMatrixPart;
 import com.glodblock.github.extendedae.common.tileentities.matrix.TileAssemblerMatrixBase;
@@ -13,6 +14,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Iterator;
+
 /** 原版玻璃加载后加入超级矩阵的延迟重算队列。 */
 @Mixin(value = TileAssemblerMatrixBase.class, remap = false)
 public abstract class TileAssemblerMatrixBaseMixin {
@@ -23,6 +26,17 @@ public abstract class TileAssemblerMatrixBaseMixin {
                 && glass instanceof SuperAssemblerMatrixPart
                 && glass.getLevel() instanceof ServerLevel serverLevel) {
             SuperAssemblerMatrixCalculator.scheduleRecalculate(serverLevel, glass.getBlockPos());
+        }
+    }
+
+    /** 让所有超级矩阵部件返回同一节点集合，确保整个多方块只消耗一个频道。 */
+    @Inject(method = "getMultiblockNodes", at = @At("HEAD"), cancellable = true)
+    private void eap$getSuperMatrixMultiblockNodes(CallbackInfoReturnable<Iterator<IGridNode>> cir) {
+        if ((Object) this instanceof SuperAssemblerMatrixPart part) {
+            var cluster = part.eap$getSuperMatrixCluster();
+            if (cluster != null) {
+                cir.setReturnValue(cluster.getGridNodes());
+            }
         }
     }
 
